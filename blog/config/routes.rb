5e31fdb14ade65b57cb083dd87f0bdcb642ca079
RhinoLee <picklerhino@gmail.com>
2021-01-21 16:03:11 +0800

    add 'C' of 'CRUD'

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get '/articles', to: 'articles#index'
-
-  resources :posts, shallow: true do
-    resources :comments
-  end
+  # get '/articles', to: 'articles#index'
+  # get '/articles/:id', to: 'articles#show'
+  
+  resources :articles
+  
 end
